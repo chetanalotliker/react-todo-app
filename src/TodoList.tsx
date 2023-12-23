f31a5c9c94dc9e047335003418c546f2a04f2ca5
chetana.lotliker <chetanalotliker@gmail.com>
2023-12-23 21:59:01 +0530

    Update Heading of the TODO List

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import { TodoListItem } from "./TodoListItem";
 import { Todo, ToggleTodo } from "./types";
 
@@ -8,10 +9,13 @@ interface Props {
 
 export const TodoList: React.FC<Props> = ({todos,toggleTodo})=>{
     return (
+      <Fragment>
+      <h2>TODO List</h2>
         <ul>
           {todos.map((todo) => (
             <TodoListItem key={todo.text} todo={todo} toggleTodo={toggleTodo} />
           ))}
         </ul>
+        </Fragment>
       );
 }
